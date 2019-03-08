e2b3ca4f664d3433528f345581830a7ab415dea9
118403064 <118403064@BISB53VL.bis.ad.ucc.ie>
2019-03-08 10:30:00 +0000

    First commit

--- index.php
+++ index.php
@@ -1,17 +1,68 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<html lang="en">
+<head>
+<title>Home Page</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+/* Style the header */
+.header {
+ background-image: url("pic.jpg");
+  padding: 70px;
+  text-align: center; 
+  
+ 
+}
+
+/* Style the top navigation bar */
+.topnav {
+  overflow: hidden;
+  background-color: black
+}
+
+/* Style the topnav links */
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change color on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: white;
+}
+body {
+  color: white;
+}
+
+h1 {
+  color: white;
+}
+</style>
+</head>
+<body>
+
+<div class="header">
+  <h1>Tyers' Consultancy</h1>
+</div>
+
+<div class="topnav">
+  <a href="http://localhost:8000/CV.html">My CV</a>
+  <a href="http://localhost:8000/tiltedpage_scroll-master/demo/tiltedpage_scroll_demo.html">Interests</a>
+  <a href="http://localhost:8000/tiltedpage_scroll-master/demo/Consultancy.html">Consultancy Services</a>
+</div>
+
+</body>
 </html>
